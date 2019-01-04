b726c5eb73f39eb4d7150eebaa536ff830d7b258
Steve Gutz <sgutz@hubspot.com>
2019-01-04 10:30:50 -0500

    Copy changes from maven-shared

--- src/main/java/org/apache/maven/shared/dependency/analyzer/asm/ConstantPoolParser.java
+++ src/main/java/org/apache/maven/shared/dependency/analyzer/asm/ConstantPoolParser.java
@@ -153,7 +153,13 @@ public class ConstantPoolParser
         Set<String> result = new HashSet<String>();
         for ( Integer aClass : classes )
         {
-            result.add( stringConstants.get( aClass ) );
+            String className = stringConstants.get( aClass );
+
+            // filter out things from the default package, probably a false-positive
+            if ( isImportableClass( className ) )
+            {
+                result.add( className );
+            }
         }
         return result;
     }
@@ -188,5 +194,10 @@ public class ConstantPoolParser
         }
         ( (Buffer) buf ).limit( oldLimit );
         return sb.toString();
+    }
+
+    private static boolean isImportableClass( String className )
+    {
+        return className.indexOf( '/' ) != -1;
     }
 }
